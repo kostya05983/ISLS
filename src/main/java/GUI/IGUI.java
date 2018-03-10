bc786682c646895d4d4ac9cdcbf61ae30dfce0df
kostya05983 <kostya05983@gmail.com>
2018-03-10 17:17:20 +0700

    add ICUI

---
+++ src/main/java/GUI/IGUI.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import SQL.Lib.Column;
+import SQL.Parser.SelectorRequest;
+
+public interface IGUI {
+    void setAllCoumns(Column[] columns);
+    void setTextOutput(String textOutput);
+}
